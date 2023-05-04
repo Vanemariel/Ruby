df55f7b3c544b7b58b5bf06c5675d70370316e82
Vanesa Herrera <vanesaailin.vh@gmail.com>
2023-05-04 16:19:42 -0300

    Agregar archivos de proyecto.

---
+++ Ruby.rb
@@ -0,0 +1,19 @@
+# Declaración de variables
+lista_numeros = [5, 10, 15, 20, 25]
+nombre = "Vane"
+
+# Estructura de control for
+for numero in lista_numeros
+  if numero % 2 == 0
+    puts "#{numero} es un número par."
+  else
+    puts "#{numero} es un número impar."
+  end
+end
+
+# Estructura de control if-else
+if nombre == "Vane"
+  puts "Hola, Vane!"
+else
+  puts "Hola, desconocido!"
+end
